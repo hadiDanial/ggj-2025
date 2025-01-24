7c395ea9b482437f614b43a99dd3c41a2eec69c0
itaisinger <itai5000si2@gmail.com>
2025-01-24 18:32:16 +0200

    ost manager done

--- GGJ25/Assets/Scripts/Sound/Music/OstManager.cs
+++ GGJ25/Assets/Scripts/Sound/Music/OstManager.cs
@@ -9,19 +9,23 @@ public class OstManager : MonoBehaviour
     [SerializeField] AudioSource ostHappy;
     [SerializeField] AudioSource ostRain;
     [SerializeField] float switchSpd = 0.02f;
-    private AudioSource[] sources;
+    [SerializeField] float rainMaxVolDis = 4;
+    [SerializeField] float rainMinVolDis = 20f;
+    [SerializeField] float maxRainVol = 0.6f;
+ 
+    private AudioSource[] sources = new AudioSource[3];
 
     public enum OSTS{
         sad,
         happy,
         rain,
     }
-    private float[] volumesDest = {0f,0f,0f};
+    private float[] volumesDest = {0f,0f,1f};
     private float rainMult = 1f;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         sources[(int)OSTS.happy] = ostHappy;
         sources[(int)OSTS.sad] = ostSad;
@@ -30,15 +34,16 @@ public class OstManager : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        for(int i=0; i < 3; i++){
-            sources[i].GetComponent<AudioSource>().volume = approach(sources[i].GetComponent<AudioSource>().volume, volumesDest[i], switchSpd);
-        }
+    {  
+        sources[0].volume = approach(sources[0].volume, volumesDest[0]*(1-rainMult), switchSpd);
+        sources[1].volume = approach(sources[1].volume, volumesDest[1]*(1-rainMult), switchSpd);
+        sources[2].volume = approach(sources[2].volume, volumesDest[2], switchSpd);
+        Debug.Log(sources[0].volume + ", " + sources[1].volume + ", " + sources[2].volume + ", rainmult: " + rainMult);
     }
 
     public void SetOst(OSTS ostIndex){
-        volumesDest[0] = 0f;
-        volumesDest[1] = 0f;
+        volumesDest[(int)OSTS.happy] = 0f;
+        volumesDest[(int)OSTS.sad] = 0f;
         volumesDest[(int)ostIndex] = 1f;
     }
 
@@ -50,4 +55,9 @@ public class OstManager : MonoBehaviour
 
     public void SetHappy(){SetOst(OSTS.happy);}
     public void SetSad(){SetOst(OSTS.sad);}
+
+    public void updateDistanceToWindow(float distance){
+        rainMult = Math.Clamp(-(distance-rainMaxVolDis)/(rainMaxVolDis-rainMinVolDis),0,maxRainVol);
+        volumesDest[2] = rainMult;
+    }
 }
