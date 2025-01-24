a99b2d361411fff8ceea1c7b4f423ffea469f5a8
Hadi <hadidanial97@gmail.com>
2025-01-24 13:22:26 +0200

    FixableObject collider

--- GGJ25/Assets/Scripts/Mechanics/FixableObject.cs
+++ GGJ25/Assets/Scripts/Mechanics/FixableObject.cs
@@ -15,25 +15,68 @@ public class FixableObject : Cleanable
     [SerializeField] private Ease fadeInEase = Ease.InOutExpo;
 
     private Sequence sequence;
+    private Collider2D collider2D;
 
     private void Awake()
     {
-        brokenObject.color = new Color(1, 1, 1, 1);
-        fixedObject.color = new Color(1, 1, 1, 0);
+        SetDefaultColors();
+        collider2D = GetComponent<Collider2D>();
+        if (collider2D == null)
+        {
+            Debug.LogError($"Missing Collider2D on {name}", gameObject);
+        }
+
+        collider2D.isTrigger = true;
+    }
+
+    private void SetDefaultColors()
+    {
+        Color c1 = brokenObject.color;
+        Color c2 = fixedObject.color;
+        c1.a = 1;
+        c2.a = 0;
+        brokenObject.color = c1;
+        fixedObject.color = c2;
     }
 
     [ContextMenu("Clean")]
     public override void Clean()
     {
+        if (sequence != null)
+        {
+            KillTween();
+        }
         sequence = DOTween.Sequence();
         sequence
             .Append(brokenObject.DOFade(0, fadeOutDuration).SetEase(fadeOutEase))
+            .Join(brokenObject.transform.DOScale(Vector3.zero, fadeOutDuration * 2f))
             .AppendInterval(intervalDuration)
-            .Append(fixedObject.DOFade(1, fadeOutDuration).SetEase(fadeInEase));
+            .Append(fixedObject.DOFade(1, fadeInDuration).SetEase(fadeInEase))
+            .Join(brokenObject.transform.DOScale(Vector3.one, fadeInDuration * 0.5f));
+        collider2D.enabled = false;
+        IsClean = true;
     }
 
     [ContextMenu("Reset Clean")]
     public override void ResetCleanable()
     {
+        KillTween();
+        SetDefaultColors();
+        collider2D.enabled = true;
+        IsClean = false;
+    }
+
+    private void KillTween()
+    {
+        sequence.Kill();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(IsClean) return;
+        if(other.TryGetComponent<Controller2D>(out Controller2D controller))
+        {
+            Clean();
+        }
     }
 }
